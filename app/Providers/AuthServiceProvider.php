a2c77dca052534063d4c15a2650f2e809aaccb1e
nachosk1 <104322849+nachosk1@users.noreply.github.com>
2023-04-30 13:38:02 -0400

    confimar cuenta a travez de correo MailTrap
    Para este proceso hay que crearse una cuenta de MailTrap y modificar el AuthServiceProvider.php para cambiar los textos a español, además, escribir el siguiente comando para modificar el mensaje el ingles a español la cual no se puede cambiar a través de lo dicho anteriormente.
    php artisan vendor:publish --tag=laravel-notifications
    Ingresando esto en el comando te crea un archivo dentro de una carpeta llamada email.blade, alli se podrá modificar el mensaje a español.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\HtmlString;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -19,8 +23,18 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * Register any authentication / authorization services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        VerifyEmail::$toMailCallback = function ($notifiable, $verificationUrl) {
+            return (new MailMessage)
+                ->subject(Lang::get('Verificar Cuenta'))
+                ->greeting(Lang::get("Hola ") . $notifiable->name)
+                ->line(Lang::get('Tu cuenta esta casi lista, solo debes presionar el enlace a continuación'))
+                ->action(Lang::get('Confirmar Cuenta'), $verificationUrl)
+                ->line(Lang::get('Si no creaste esta cuenta, puedes ignorar este mensaje'))
+                ->salutation(new HtmlString(
+                    Lang::get("Devjobs.") . '<br>' . '<strong>' . Lang::get("Nuestro Equipo") . '</strong>'
+                ));
+        };
     }
 }
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,21 +1,21 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+        {{ __('Es necesario confirmar tu cuenta antes de continuar, revisa tu Email y presiona sobre el enlace de confirmación') }}
     </div>
 
     @if (session('status') == 'verification-link-sent')
         <div class="mb-4 font-medium text-sm text-green-600">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+            {{ __('Hemos enviado un nuevo Email de confirmación a la cuenta que colocaste en el registro') }}
         </div>
     @endif
 
-    <div class="mt-4 flex items-center justify-between">
+    <div class="mt-4 flex items-center justify-between md:flex-row flex-col">
         <form method="POST" action="{{ route('verification.send') }}">
             @csrf
 
             <div>
                 <x-primary-button>
-                    {{ __('Resend Verification Email') }}
+                    {{ __('Enviar Email de confirmación') }}
                 </x-primary-button>
             </div>
         </form>
@@ -23,8 +23,8 @@
         <form method="POST" action="{{ route('logout') }}">
             @csrf
 
-            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                {{ __('Log Out') }}
+            <button type="submit" class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                {{ __('Cerra Sesión') }}
             </button>
         </form>
     </div>
